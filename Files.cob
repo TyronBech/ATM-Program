2bfea0b7838c97f6225798b40db0d88fd8f0945c
Tyron Bechayda <tyronbechayda1112@gmail.com>
2024-01-21 14:19:53 +0800

    ADDED PRACTICE FILE

---
+++ Files.cob
@@ -0,0 +1,20 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM YOUR-PROGRAM-NAME.
